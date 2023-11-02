732179412b67cd9df964fe4b70fd9f48dfaabc95
alexander.kudlay <alexander.kudlay@x2sy.com>
2023-11-02 15:17:51 +0200

    Lesson09HomeWork17ExchangeRate2

--- src/Bank.java
+++ src/Bank.java
@@ -1,25 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Bank {
-        private String name;
-        private double exchangeRate;
+public void Kyrs (){
+    Random random = new Random();
+    String[] bankNames = { "ПриватБанк", "Ощадбанк", "Укрексімбанк", "Сенс Банк", "Сітібанк", "Райффайзен Банк",
+            "УКРСИББАНК", "ПУМБ", "КРЕДІ АГРІКОЛЬ БАНК", "КРЕДОБАНК", "ТАСКОМБАНК", "БТА Банк", "ОТП БАНК", "ЮНЕКС БАНК"
+    };
 
-        public Bank(String name, double exchangeRate) {
-            this.name = name;
-            this.exchangeRate = exchangeRate;
-        }
+    double[] exchangeRate = new double[14];
+    double[] checkGoodExchange = new double[14];
+        for (int i = 0; i < exchangeRate.length; i++) {
+        exchangeRate[i] = (random.nextInt(500)+3500) / 100.0;
+        //exchangeRate[i] = random.nextInt(5);
+        checkGoodExchange[i] = exchangeRate[i];
+    }
+        Arrays.sort(checkGoodExchange);
 
-        public String getName() {
-            return name;
+        for (int i = 0; i < exchangeRate.length; i++) {
+        //  System.out.println(checkMaxExchange[i]);
+        //  System.out.println();
+        if (exchangeRate[i] == checkGoodExchange[checkGoodExchange.length - 1]) {
+            System.out.println("Банк з найкращим курсом обміну доларів на гривні: " + bankNames[i] + ": " + exchangeRate[i]);
         }
-
-        public double getExchangeRate() {
-            return exchangeRate;
-        }
-
-        @Override
-        public String toString() {
-            return name + " - Курс: " + exchangeRate;
-        }
+    }}
     }
 
--- src/ExchangeRate.java
+++ src/ExchangeRate.java
@@ -3,30 +3,7 @@ import java.util.Random;
 
 public class ExchangeRate {
     public static void main (String[] args){
-        Random random = new Random();
-        String[] bankNames = { "ПриватБанк", "Ощадбанк", "Укрексімбанк", "Сенс Банк", "Сітібанк", "Райффайзен Банк",
-                "УКРСИББАНК", "ПУМБ", "КРЕДІ АГРІКОЛЬ БАНК", "КРЕДОБАНК", "ТАСКОМБАНК", "БТА Банк", "ОТП БАНК", "ЮНЕКС БАНК"
-        };
-//        int[] nameBank = new int[14];
-        double[] exchangeRate = new double[14];
-        double[] checkGoodExchange = new double[14];
-        for (int i = 0; i < exchangeRate.length; i++) {
-            exchangeRate[i] = (random.nextInt(500)+3500) / 100.0;
-            //exchangeRate[i] = random.nextInt(5);
-            checkGoodExchange[i] = exchangeRate[i];
-                    }
-        Arrays.sort(checkGoodExchange);
-        for (int i = 0; i < exchangeRate.length; i++) {
-          //  System.out.println(checkMaxExchange[i]);
-          //  System.out.println();
-            if (exchangeRate[i] == checkGoodExchange[checkGoodExchange.length - 1]) {
-                System.out.println("Банк з найкращим курсом обміну доларів на гривні: " + bankNames[i] + ": " + exchangeRate[i]);
-            }
-        }
-//        for (int i = 0; i<exchangeRate.length; i++){
-//            if (exchangeRate[i] == checkMaxExchange[checkMaxExchange.length-1]) {
-//                System.out.println("Банк з найкращим курсом обміну доларів на гривні: " + bankNames[i] + ": " + exchangeRate[i]);
-//            }
-//        }
+        Bank bank = new Bank();
+        bank.Kyrs();
     }
 }
